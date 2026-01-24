e402a4819393001270d4296206d82c432cc71fc9
phessophissy <phessophissy@proton.me>
2026-01-24 15:06:57 +0100

    feat(frontend): add component barrel exports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,18 @@
+// Component barrel exports
+export { default as Badge } from './Badge';
+export { default as Button } from './Button';
+export { default as Card, CardHeader, CardBody, CardFooter } from './Card';
+export { default as CollectibleCard } from './CollectibleCard';
+export { default as Dropdown } from './Dropdown';
+export { default as FloatingBalls } from './FloatingBalls';
+export { default as Header } from './Header';
+export { default as Hero } from './Hero';
+export { default as Input } from './Input';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as Modal } from './Modal';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Skeleton, CardSkeleton, CollectibleCardSkeleton } from './Skeleton';
+export { default as Tabs } from './Tabs';
+export { default as Tooltip } from './Tooltip';
+export { default as Avatar, AvatarGroup } from './Avatar';
+export { ToastProvider, useToast } from './Toast';
